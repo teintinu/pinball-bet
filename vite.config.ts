ea650f65330884368ef36d4597a817c92ffefada
José Fernando Tolentino <josefernandotolentino@gmail.com>
2023-12-10 03:09:08 +0000

    fix vite/base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pinball-bet.git/",
+  base: "/pinball-bet/",
   build: {
     outDir: './docs',
     emptyOutDir: true,
